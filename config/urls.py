fc17ba350a2a05911865d93be73bb4a8928479a3
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-06-08 01:11:52 +0900

    fix: delete re_path

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('katadzuke_app_v1.urls')),
-    re_path('', RedirectView.as_view(url='/'))
+    # re_path('', RedirectView.as_view(url='/'))
 ]
