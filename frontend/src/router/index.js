93a1ff7517c118d566833fad6d3dab879bf8d940
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-06-08 01:03:32 +0900

    fix: delete Base URL

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,15 +11,7 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/sign-up",
@@ -35,7 +27,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes,
 });
 
