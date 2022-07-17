a7724f10e3e8b760955e2bf2acaced21149fba93
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-07-17 22:20:35 +0900

    add replace_current_cloudinary_photo_with_posted_photo to decrease amount of code of views.py

---
+++ katadzuke_app_v1/utils.py
@@ -0,0 +1,50 @@
+import base64, cv2, math, cloudinary, numpy as np
+
+
+def calc_percent_of_floors_of_photo(base64_content, floor_hue_ranges):
+
+        np_img = np.asarray(
+            bytearray(base64.b64decode(base64_content.split(",")[1])), dtype="uint8"
+        )
+        hsv = cv2.cvtColor(cv2.imdecode(np_img, cv2.IMREAD_COLOR), cv2.COLOR_BGR2HSV)
+
+        total_num_of_floor_pixels = 0
+        for h in range(hsv.shape[0]):
+            for w in range(hsv.shape[1]):
+                hue_value = hsv[h, w, 0]
+                
+                for floor_hue_range in floor_hue_ranges:
+                    min_hue = floor_hue_range.min_hue
+                    max_hue = floor_hue_range.max_hue
+                    
+                    if max_hue >= hue_value and hue_value >= min_hue:
+                        total_num_of_floor_pixels += 1
+                    elif max_hue >= hue_value + 360 and hue_value + 360 >= min_hue:
+                        total_num_of_floor_pixels += 1
+        
+        total_num_of_pixels = hsv.shape[0] * hsv.shape[1]
+
+        return math.floor(total_num_of_floor_pixels / total_num_of_pixels * 100)
+
+def upload_photo_to_cloudinary(base64_content):
+
+    response = cloudinary.uploader.upload(file=base64_content)
+
+    return response["public_id"], response["url"]
+
+
+def destroy_photo_from_cloudinary(photo_public_id):
+
+    cloudinary.uploader.destroy(photo_public_id)
+
+
+def replace_current_cloudinary_photo_with_posted_photo(public_id_of_current_cloudinary_photo, base64_content_of_posted_photo):
+
+    if public_id_of_current_cloudinary_photo is not None:
+        destroy_photo_from_cloudinary(public_id=public_id_of_current_cloudinary_photo)
+
+    public_id_of_photo_uploaded_to_cloudinary, url_of_photo_uploaded_to_cloudinary = upload_photo_to_cloudinary(
+        base64_content=base64_content_of_posted_photo
+    )
+
+    return public_id_of_photo_uploaded_to_cloudinary, url_of_photo_uploaded_to_cloudinary
--- katadzuke_app_v1/views.py
+++ katadzuke_app_v1/views.py
@@ -1,6 +1,11 @@
 from importlib.util import spec_from_file_location
 from logging import raiseExceptions
-from .serializers import UserSerializer, RoomPhotoSerializer, RewardSerializer, FloorPhotoSerializer
+from .serializers import (
+    UserSerializer,
+    RoomPhotoSerializer,
+    RewardSerializer,
+    FloorPhotoSerializer,
+)
 from .models import RoomPhoto, Reward, FloorPhoto, FloorHueRange
 from rest_framework import views, status
 from rest_framework.response import Response
@@ -8,14 +13,15 @@ from rest_framework.parsers import MultiPartParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
 import numpy as np
 from .room_vision import (
-    calc_percent_of_floors,
-    upload_photo_to_cloudinary,
-    destroy_photo_from_cloudinary,
     reflect_room_photo_score_to_amount_of_money,
     remove_reflection_of_room_photo_score_from_amount_of_money,
     calc_upload_floor_photo_hue_cnt_list,
     merge_floor_hue_ranges_into_upload_floor_photo_hue_cnt_list,
-    create_new_hue_ranges_model
+    create_new_hue_ranges_model,
+)
+from .utils import (
+    calc_percent_of_floors_of_photo,
+    replace_current_cloudinary_photo_with_posted_photo,
 )
 import datetime
 from django.db.models import Q
@@ -35,31 +41,23 @@ class RoomPhotoCreateAPIView(views.APIView):
         serializer = RoomPhotoSerializer(instance=room_photo, data={}, partial=True)
         serializer.is_valid(raise_exception=True)
 
-        if room_photo.photo_public_id is not None:
-
-            destroy_photo_from_cloudinary(room_photo.photo_public_id)
-
-        public_id, url = upload_photo_to_cloudinary(
-            request.data["roomPhotoBase64Content"]
+        (
+            public_id_of_photo_uploaded_to_cloudinary,
+            url_of_photo_uploaded_to_cloudinary,
+        ) = replace_current_cloudinary_photo_with_posted_photo(
+            public_id_of_current_cloudinary_photo=room_photo.photo_public_id,
+            base64_content_of_posted_photo=request.data["roomPhotoBase64Content"],
         )
 
-        floor_hue_ranges = FloorHueRange.objects.filter(user=request.user)
-
-        floor_hue_ranges_list = []
-
-        for floor_hue_range in floor_hue_ranges:
-            floor_hue_ranges_list.append(floor_hue_range.min_hue)
-            floor_hue_ranges_list.append(floor_hue_range.max_hue)
-
-        percent_of_floors = calc_percent_of_floors(
-            request.data["roomPhotoBase64Content"],
-            floor_hue_ranges_list
+        percent_of_floors = calc_percent_of_floors_of_photo(
+            base64_content=request.data["roomPhotoBase64Content"],
+            floor_hue_ranges=FloorHueRange.objects.filter(user=request.user),
         )
 
         serializer.save(
-            photo_public_id=public_id,
+            photo_public_id=public_id_of_photo_uploaded_to_cloudinary,
             percent_of_floors=percent_of_floors,
-            photo_url=url,
+            photo_url=url_of_photo_uploaded_to_cloudinary,
         )
 
         return Response(serializer.data, status.HTTP_200_OK)
@@ -79,22 +77,24 @@ class RewardThisMonthUpdateAPIView(views.APIView):
         serializer = RewardSerializer(instance=reward_this_month, data={}, partial=True)
 
         add_amount_this_month = reflect_room_photo_score_to_amount_of_money(
-                request.user.threshould_fine_score,
-                request.user.threshould_reward_score,
-                request.user.amount_of_fine,
-                request.user.amount_of_reward,
-                request.data["new_room_photo_score"],
-            )
+            request.user.threshould_fine_score,
+            request.user.threshould_reward_score,
+            request.user.amount_of_fine,
+            request.user.amount_of_reward,
+            request.data["new_room_photo_score"],
+        )
 
         if request.data["prev_room_photo_score"] is not None:
-            add_amount_this_month -= remove_reflection_of_room_photo_score_from_amount_of_money(
-                request.user.threshould_fine_score,
-                request.user.threshould_reward_score,
-                request.user.amount_of_fine,
-                request.user.amount_of_reward,
-                request.data["prev_room_photo_score"],
+            add_amount_this_month -= (
+                remove_reflection_of_room_photo_score_from_amount_of_money(
+                    request.user.threshould_fine_score,
+                    request.user.threshould_reward_score,
+                    request.user.amount_of_fine,
+                    request.user.amount_of_reward,
+                    request.data["prev_room_photo_score"],
+                )
             )
-            
+
         print(add_amount_this_month)
         serializer.is_valid(raise_exception=True)
 
@@ -118,6 +118,7 @@ class RoomPhotoListAPIView(views.APIView):
 
         return Response(serializer.data, status.HTTP_200_OK)
 
+
 class FloorPhotoGetAPIView(views.APIView):
 
     permission_classes = [IsAuthenticated]
@@ -130,40 +131,44 @@ class FloorPhotoGetAPIView(views.APIView):
 
         return Response(serializer.data, status.HTTP_200_OK)
 
+
 class FloorPhotoUploadAPIView(views.APIView):
 
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk, *args, **kwargs):
-        
+
         # TODO: 認可追加
         floor_photo = FloorPhoto.objects.get(pk=pk)
         serializer = FloorPhotoSerializer(instance=floor_photo, data={}, partial=True)
         serializer.is_valid(raise_exception=True)
 
-        if floor_photo.photo_public_id is not None:
-
-            destroy_photo_from_cloudinary(floor_photo.photo_public_id)
-
-        public_id, url = upload_photo_to_cloudinary(
-            request.data["floorPhotoBase64Content"]
+        (
+            public_id_of_photo_uploaded_to_cloudinary,
+            url_of_photo_uploaded_to_cloudinary,
+        ) = replace_current_cloudinary_photo_with_posted_photo(
+            public_id_of_current_cloudinary_photo=floor_photo.photo_public_id,
+            base64_content_of_posted_photo=request.data["floorPhotoBase64Content"],
         )
 
         serializer.save(
-            photo_public_id=public_id,
-            photo_url=url,
+            photo_public_id=public_id_of_photo_uploaded_to_cloudinary,
+            photo_url=url_of_photo_uploaded_to_cloudinary,
         )
 
-
-
-
         floor_hue_ranges = FloorHueRange.objects.filter(user=request.user)
-        hue_floor_pixel_cnt_list = merge_floor_hue_ranges_into_upload_floor_photo_hue_cnt_list(floor_hue_ranges, calc_upload_floor_photo_hue_cnt_list(request.data["floorPhotoBase64Content"]))
+        hue_floor_pixel_cnt_list = (
+            merge_floor_hue_ranges_into_upload_floor_photo_hue_cnt_list(
+                floor_hue_ranges,
+                calc_upload_floor_photo_hue_cnt_list(
+                    request.data["floorPhotoBase64Content"]
+                ),
+            )
+        )
 
         floor_hue_ranges.delete()
 
         create_new_hue_ranges_model(hue_floor_pixel_cnt_list, request.user)
-
 
         return Response(serializer.data, status.HTTP_200_OK)
 
